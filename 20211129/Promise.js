17e288bfe3260c41ba1fdd0bd6ef808ac99cf4a5
Allenyan <973122310@qq.com>
2021-11-30 23:53:27 +0800

    Promise.prototype.then

--- 20211129/Promise.js
+++ 20211129/Promise.js
@@ -10,25 +10,26 @@
             4.resolve和reject函数用于改变状态
     */
     function Promise(excutor) {
+        let _this = this;
         /*状态初始化*/
         this.status = 'pending';
         this.data = undefined;
         //promise可能先产生回调函数，再改变状态;也可能先改变状态，再产生回调函数;因此用一个数组来存
         this.callbacks = [];//每个回调函数{onResolved(){},onRejected(){}}
 
-        /*执行器函数中调用resolve函数(注意this指向)*/
-        const resolve = (value) => {
+        /*执行器函数中调用resolve函数(注意_this指向)*/
+        function resolve(value) {
             //3.promise状态只能改一次
-            if (this.status !== 'pending') return;
+            if (_this.status !== 'pending') return;
 
             //执行resolve方法有两个事情要做：
             //1.改变promise对象内部参数   
-            this.status = 'resolved';
-            this.data = value;
+            _this.status = 'resolved';
+            _this.data = value;
             //2.如果先产生回调函数，由于此时改变了状态，满足回调函数触发的两个条件所以要异步执行then中的回调函数
-            if (this.callbacks.length > 0) {
+            if (_this.callbacks.length > 0) {
                 setTimeout(function () {//模拟异步执行，但是不准确，因为定时器是宏队列，但是promise回调时微队列
-                    this.callbacks.forEach(cbObj => {
+                    _this.callbacks.forEach(cbObj => {
                         cbObj.onResolved(value);
                     })
                 })
@@ -36,11 +37,11 @@
         }
         /*执行器函数中调用reject函数*/
         function reject(reason) {
-            this.status = 'rejected';
-            this.data = reason;
-            if (this.callbacks.length > 0) {
+            _this.status = 'rejected';
+            _this.data = reason;
+            if (_this.callbacks.length > 0) {
                 setTimeout(function () {
-                    this.callbacks.forEach(cbObj => {
+                    _this.callbacks.forEach(cbObj => {
                         cbObj.onRejected(reason);
                     })
                 })
@@ -56,9 +57,44 @@
     }
 
     Promise.prototype.then = (onResolved, onRejected) => {
-        
-        /*先产生回调函数,后改变状态*/
-        this.callbacks.push({onResolved,onRejected});
+        let _this = this;
+        return new Promise((resolve, reject) => {
+            /*封装改变promise状态的函数*/
+            function handlePromise(cb) {
+                try {
+                    const result = cb(_this.data);
+                    console.log(result, '1111');
+                    if (result instanceof Promise) {
+                        result.then(resolve, reject);
+                    } else {
+                        resolve(_this.data)
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            }
+            /*先产生回调函数,后改变状态*/
+            if (_this.status === 'pending') {
+                _this.callbacks.push({
+                    onResolved() {
+                        handlePromise(onResolved);
+                    },
+                    onRejected() {
+                        handlePromise(onRejected);
+                    }
+                });
+            }
+            /*先改变状态，后产生回调函数，同步执行then的时候，异步调用回调*/
+            else if (_this.status === 'resolved') {
+                setTimeout(() => {
+                    handlePromise(onResolved);
+                })
+            } else {
+                setTimeout(() => {
+                    handlePromise(onRejected);
+                })
+            }
+        })
     }
 
     window.Promise = Promise;
